611185b42832b1f62253de2fab24a28b8a880e2f
TLY-STUDIO-CODE <andryrazafindrabe@yahoo.com>
2026-01-14 23:20:11 +0300

    feat: implement contact form with Formspree and CV download

--- app/contact/page.jsx
+++ app/contact/page.jsx
@@ -1,7 +1,50 @@
 "use client";
 import { motion } from "framer-motion";
+import { Label } from "@/components/ui/label";
+import { Input } from "@/components/ui/input";
+import { Textarea } from "@/components/ui/textarea";
+import React, { useState } from "react";
+
+import { 
+  Select, 
+  SelectTrigger, 
+  SelectValue, 
+  SelectContent, 
+  SelectItem 
+} from "@/components/ui/select";
+
+import { HiOutlineMapPin, HiOutlineArrowLongRight } from "react-icons/hi2"
+import { HiOutlinePhone, HiOutlineMail } from "react-icons/hi"
 
 const Contact = () => {
+  const [isSuccess, setIsSuccess] = useState(false);
+  const [isLoading, setIsLoading] = useState(false);
+
+  async function handleSubmit(event) {
+    event.preventDefault();
+    setIsLoading(true); // Bloquer le bouton
+
+    const formData = new FormData(event.target);
+    
+    // Note: FormData récupère automatiquement les Input/Textarea avec un attribut 'name'
+    const response = await fetch("https://formspree.io/f/xlggdgqo", {
+      method: "POST",
+      body: formData,
+      headers: {
+        'Accept': 'application/json'
+      }
+    });
+
+    if (response.ok) {
+      setIsSuccess(true);
+      setIsLoading(false);
+      event.target.reset(); 
+      setTimeout(() => setIsSuccess(false), 5000);
+    } else {
+      setIsLoading(false);
+      alert("Une erreur est survenue, veuillez réessayer.");
+    }
+  }
   return (
     <motion.section  
       initial={{ opacity: 0 }} 
@@ -9,8 +52,146 @@ const Contact = () => {
         opacity: 1, 
         transition: { delay: 2.4, duration: 0.4, ease: "easeIn" }, 
       }}
+      className="h-screen flex items-center py-24 xl:py-0"
     >
-      contact
+
+    <div className="container mx-auto w-full h-full flex flex-col items-center xl:justify-center xl:overflow-hidden scrollbar scrollbar-thumb-accent scrollbar-track-accent/5 overflow-y-scroll 
+    xl:overflow-y-visible">
+      <div className="w-full">
+        <div className="flex flex-col xl:flex-row gap-6">
+          {/* info text */}
+          <div className="flex-1 xl:w-[600px] flex flex-col gap-12">
+            {/* text */}
+            <div>
+              <h2 className="h2 mb-6">
+                Me <span className="text-accent">contacter</span>
+              </h2>
+              <p className="max-w-[460px]">
+                Un projet en tête ou simplement envie d'échanger ? 
+                Je suis toujours ouvert à de nouvelles opportunités 
+                et à de belles rencontres.
+              </p>
+            </div>
+            {/* info */}
+            <div className="flex flex-col gap-8 mb-6 xl:mb-0">
+              {/* phone */}
+              <div className="flex items-center gap-4 text-lg">
+                <span className="text-accent">
+                  <HiOutlinePhone className="text-2xl"/>
+                </span>
+                <span>+261 34 17 332 48</span> 
+              </div>
+              {/* email */}
+              <div className="flex items-center gap-4 text-lg">
+                <span className="text-accent">
+                  <HiOutlineMail className="text-2xl"/>
+                </span>
+                <span>tsilaky01@gmail.com</span> 
+              </div>
+              {/* location */}
+              <div className="flex items-center gap-4 text-lg">
+                <span className="text-accent">
+                  <HiOutlineMapPin className="text-2xl"/>
+                </span>
+                <span>Lot III 143b Alarobia</span> 
+              </div>
+            </div>
+          </div>
+          {/* form */}
+          <div className="flex-1">
+            <form onSubmit={handleSubmit} className="flex flex-col gap-6 items-start">
+              {/* First and last name */}
+              <div className="flex flex-col xl:flex-row gap-6 w-full">
+                <div className="w-full">
+                  <Label htmlFor="firstname">
+                    Prénom <span className="text-accent">*</span>
+                  </Label>
+                  <Input 
+                      id="firstname" 
+                      name="firstname" 
+                      placeholder="Votre prénom" 
+                      required 
+                    />
+                </div>
+                <div className="w-full">
+                  <Label htmlFor="lastname">
+                    Nom <span className="text-accent">*</span>
+                  </Label>
+                  <Input 
+                      id="lastname" 
+                      name="lastname" 
+                      placeholder="Votre Nom" 
+                      required 
+                    />
+                </div>
+              </div>
+              {/* email */}
+              <div className="w-full">
+                <Label htmlFor="email">
+                  Adresse e-mail <span className="text-accent">*</span>
+                </Label>
+                <Input 
+                  id="email" 
+                  name="email" 
+                  type="email"
+                  placeholder="adresse@gmail.com" 
+                  required
+                  />
+              </div>
+              {/* select */}
+              <div className="w-full">
+                <Label htmlFor="service">
+                  Je suis intéressé par <span className="text-accent">*</span>
+                </Label>
+                <Select name="service" required>
+                  <SelectTrigger 
+                    id="service" 
+                    className="w-full !h-12 bg-white/5 border-white/10 px-4"
+                  >
+                    <SelectValue placeholder="Choisissez ici" />
+                  </SelectTrigger>
+                  <SelectContent className="bg-black border-white/20">
+                    <SelectItem value="webdev">Développement Web</SelectItem>
+                    <SelectItem value="uiux">Design UI/UX</SelectItem>
+                    <SelectItem value="logo">Création de logo</SelectItem>
+                  </SelectContent>
+                </Select>
+              </div>
+              {/* textarea */}
+              <div className="w-full">
+                <Label htmlFor="message">
+                  Message <span className="text-accent">*</span>
+                </Label>
+                <Textarea 
+                  id="message" 
+                  name="message" 
+                  placeholder="Écrivez votre message ici..." 
+                  className="min-h-[160px] bg-white/5 border-white/10 
+                  focus-visible:border-accent focus-visible:ring-accent 
+                  focus-visible:ring-[1px] resize-none p-4 selection:bg-accent 
+                  placeholder:text-white/50"
+                />
+              </div>
+              {/* success msg */}
+              {isSuccess && (
+                  <div className="text-accent font-medium animate-bounce bg-accent/10 p-3 rounded-md w-full text-center">
+                    ✅ Message envoyé avec succès !
+                  </div>
+                )}
+              {/* btn */}
+              <button type="submit" disabled={isLoading} className="btn btn-lg btn-accent">
+                <div className="flex items-center gap-3">
+                  <span className="font-medium">
+                      {isLoading ? "Envoi en cours..." : "Envoyer le message"}
+                    </span>
+                    {!isLoading && <HiOutlineArrowLongRight className="text-xl" />}
+                </div>
+              </button>
+            </form>
+          </div>
+        </div>
+      </div>
+    </div>
     </motion.section>
   );
 };
